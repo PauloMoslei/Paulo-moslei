26538e902994310d414f3f65f457da595455e747
paulo <paulomoslei25@gmail.com>
2025-10-16 10:02:31 -0300

    funciona

---
+++ gerenciador_web/config/projetos/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+# Create your views here.
+from .models import Projeto
+from django.http import HttpResponse
+
+def listar_projetos (request):
+    projetos_salvos = Projeto.objects.all()
+
+    contexto = {
+        'meus_projetos': projetos_salvos
+    }
+    return render(request, 'projetos/lista.html', contexto)
