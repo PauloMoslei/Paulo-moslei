c931a220b7907f53a083e169a75ee66a74f4bda8
paulo <paulomoslei25@gmail.com>
2025-10-29 11:27:39 -0300

    =lista

--- gerenciador_web/config/tarefas/models.py
+++ gerenciador_web/config/tarefas/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from projetos.models import Projeto
+
 class Tarefa(models.Model):
     titulo = models.CharField(max_length = 200)
     descricao = models.TextField(blank = True, null = True)
     data_criacao = models.DateTimeField(auto_now_add = True)
     concluida = models.BooleanField(default = False)
+    projeto = models.ForeignKey(Projeto, on_delete = models.CASCADE, null = True, blank = True)
 
     #Exibir título por padrão
     def __str__(self):
--- gerenciador_web/config/tarefas/views.py
+++ gerenciador_web/config/tarefas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
+from projetos.models import Projeto
+
 # Create your views here.
 from .models import Tarefa
 from django.http import HttpResponse
@@ -17,12 +19,15 @@ def detalhe_tarefa (request, tarefa_id):
     return render (request, 'tarefas/detalhe.html',{'tarefa':tarefa})
 
 def adicionar_tarefa(request):
+    projetos = Projeto.objects.all()
     if request.method == 'POST':
         titulo = request.POST.get('titulo')
         descricao = request.POST.get('descricao')
-        Tarefa.objects.create(titulo= titulo, descricao= descricao)
+        projeto_id = request.POST.get('projeto')
+        projeto_selecionado = Projeto.objects.get (pk = projeto_id)
+        Tarefa.objects.create(titulo=titulo, descricao=descricao, projeto=projeto_selecionado)
         return redirect('lista_tarefas')
-    return render(request, 'tarefas/form_tarefa.html')
+    return render(request, 'tarefas/form_tarefa.html', {'projetos': projetos})
 
 #metodos http
 #post: envia dados para o servidor
@@ -31,17 +36,21 @@ def adicionar_tarefa(request):
 #delete: remove recursos selecionados
 
 def alterar_tarefa(request, tarefa_id):
+    projetos = Projeto.objects.all()
     tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
     
     if request.method == 'POST':
         titulo = request.POST.get('titulo')
         descricao = request.POST.get('descricao')
+        projeto_id = request.POST.get('projeto')
         concluida = request.POST.get('concluida') == 'on' 
+        projeto_selecionado = get_object_or_404(Projeto, pk = projeto_id)
 
       
         tarefa.titulo = titulo
         tarefa.descricao = descricao
         tarefa.concluida = concluida
+        tarefa.projeto = projeto_selecionado
         
         tarefa.save()
         
@@ -49,6 +58,7 @@ def alterar_tarefa(request, tarefa_id):
 
     context = {
         'tarefa': tarefa,
+        'projetos': projetos,
     }
     return render(request, 'tarefas/form_tarefas.html', context)
 
